c19f9593345cf7388b3cc13847c406a20ca657cb
Darya van Tienhoven <daryaheleen@gmail.com>
2021-02-09 15:45:11 +0100

    tekst klein beetje aangepast

--- src/Components/data/timelineData.js
+++ src/Components/data/timelineData.js
@@ -166,7 +166,7 @@ const timelineData =[
     title: "Portfolio",
     talen: ["React", "Sass"],
     text: `Misschien een beetje vreemd om deze ook hier te delen, maar ik wilde toch beschrijven wat ik van dit project geleerd heb. Eén van de belangrijkste
-    dingen die ik geleerd heb is het dynamisch renderen van foto's voor mijn website. Voor het tijdlijn-element heb ik react-vertical-timeline gebruikt. Hiervoor 
+    dingen die ik geleerd heb is het dynamisch toevoegen van foto's aan mijn website. Voor het tijdlijn-element heb ik react-vertical-timeline gebruikt. Hiervoor 
     was het nuttig om inline te stylen, dus daar heb ik meer mee geoefend. Omdat ik ook terug keek naar oudere projecten heb ik weer Sass gebruikt (ik was dat een 
     beetje vergeten), en herontdekt hoe fijn dat is om je CSS code te structureren.`,
     gitHubRepo: "https://github.com/DHvanTienhoven/portfolio_website",
